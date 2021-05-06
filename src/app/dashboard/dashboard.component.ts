4c59fe0030a2bfbab1ceb516aee542a02cd6ed0b
Kiran Kumar Mallavarapu <kmallavarapu@corp.miraclesoft.com>
2021-05-06 18:34:36 +0530

    added disrtict data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,28 +9,54 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private http:HttpClient) { 
+  constructor(private http:HttpClient,private route: Router) { 
     
   }
 
+  serverURL = "https://my-covid-application.herokuapp.com";
+  // serverURL = "http://localhost:9595";
+
   title:"Covid DashBoard"
   stateData:any
+  districtData:any;
+  goBack:boolean=true;
+
+ 
 
   ngOnInit(): void {
     this.getStateCovidRecords();
   }
 
-  getStateCovidRecords(){
-    this.http.get("https://my-covid-application.herokuapp.com/covid19/india").subscribe(res => {
-      if (res) {
+  goBackToState(){
+    this.districtData = null;
+    this.goBack= false;
+  }
 
+  getStateCovidRecords(){
+    this.http.get(this.serverURL+"/covid19/india").subscribe(res => {
+      if (res) {
         this.stateData = res;
-       
       }
       else {
         this.stateData = [];
       }
     })
+  }
+
+  getDistrictCovidData(stateData){
+    this.http.get(this.serverURL+"/covid19/district/"+stateData.state).subscribe(res => {
+      if (res) {
+        this.districtData = res;
+      }
+      else {
+        this.districtData = [];
+      }
+    })
+  }
+
+  navigateDetails(){
+    console.log("Navigating")
+this.route.navigateByUrl("https://www.google.com");
   }
 
 }
